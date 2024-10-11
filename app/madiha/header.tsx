e122eeea137ab13eda452ee8d1f8f50b8620b3e6
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-11 19:24:41 +0500

    headers

---
+++ app/madiha/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>madiha </h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Header from './component/header/header';
+// import Header from './madiha/header';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+
+    <h1>Hello Pakistan</h1>
+      <Header></Header>
+      {/* <Header></header> */}
+    </div>
+  )
+};
+
+
+export default Navbar;
